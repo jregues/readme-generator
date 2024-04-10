ec4f1278ba2a85f72b17dbceacda7b8326ef0d2d
jregues <jregues@me.com>
2024-04-09 20:18:25 -0400

    added changes to console logs

--- JS/index.js
+++ JS/index.js
@@ -2,11 +2,12 @@
 const inquirer = require('inquirer');
 const colors = require('colors');
 const lB = colors.lightBlue;
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
 inquirer
-    .prompt(
+    .prompt([
         {
             type: 'input',
             message: 'Please enter the title for your README file',
@@ -20,7 +21,7 @@ inquirer
         {
             type: 'input',
             message: 'Please explain how to install your application',
-            name: 'intallation',
+            name: 'installation',
         },
         {
             type: 'input',
@@ -50,20 +51,27 @@ inquirer
         {
             type: 'input',
             message: 'Please enter any ways the user can contribute',
-            name: 'contribute',
+            name: 'contribution',
         },
         {
             type: 'input',
             message: 'Please put any test you made for your application',
             name: 'application',
         }
-    )
+    ])
 
 
-    .then((response) =>
-        console.log(response.title),
+    .then((response) => {
+        // console.log('bruh');
+        writeToFile(response);
+    }
+        // console.log(response.title + ''),
+        // writeToFile('log.txt', "" + response.title + '\n'),
+        // console.log('bruh'),
+        // console.log(response.title),
+        // writeToFile(response),
         // console.log("## Table of Contents"),
-        console.log(response.table),
+        // console.log(response.table),
         // console.log(`${lB("## Installation")}`),
         // console.log(`${(response.installation)}`),
         // console.log(`${lb("## Usage")}`),
@@ -80,11 +88,25 @@ inquirer
         // console.log(`${response.contribute}`),
         // console.log(`${lb("## Application")}`),
         // console.log(`${response.application}`),
-    )
+    );
+// console.log(response.title + '')
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(data) {
+    // console.log(data.title + '');
+    let file = '' + data.title + '\n' + "## Table of Contents" + '\n' + data.table + '\n' + "## Installation" + '\n' + data.installation + '\n' + "## Usage" + '\n' + data.usage + '\n' + "## Credits" + '\n' + data.credits + '\n' + "## Liscense" + '\n' + data.liscense + '\n' + "## Badges" + '\n' + data.badges + '\n' + "## Features" + '\n' + data.features + '\n' + "## Contribution" + '\n' + data.contribution + '\n' + "## Application" + '\n' + data.application;
+    console.log(file + '');
+
+    fs.writeFile('./log.txt', file, err => {
+        if (err) {
+            console.error(err);
+        } else[
+            console.log('bruh')
+        ]
+    }
+    );
+}
 
 // TODO: Create a function to initialize app
 function init() { }
